0b96f8458cc6a69fbbea8293244e2964dea3dd1a
Null Cookie Games <31669166+PeterTranz@users.noreply.github.com>
2019-09-09 12:04:24 -0400

    Button Toggle and Joystick fix
    Fixed the input for the Right joystick on xbox.
    
    Added the abillity to toggle between holding down a button or pressing it.

--- Controller Inputs/InputManager.cs
+++ Controller Inputs/InputManager.cs
@@ -13,6 +13,9 @@ using UnityEngine;
 
 public static class InputManager
 {
+    public static bool ToggleHold;
+    public static bool ToggleHoldExtras;
+
     #region Xbox Controls
     //Left and Right joysticks
     public static float X_LeftStickX() {
@@ -38,10 +41,20 @@ public static class InputManager
 
     //Joystick Buttons
     public static bool X_LeftButton() {
-        return Input.GetButtonDown("X_LeftButton");
+        if (ToggleHold) {
+            return Input.GetButton("X_LeftButton");
+        }
+        else {
+            return Input.GetButtonDown("X_LeftButton");
+        }
     }
     public static bool X_RightButton() {
-        return Input.GetButtonDown("X_RightButton");
+        if (ToggleHold) {
+            return Input.GetButton("X_RightButton");
+        }
+        else {
+            return Input.GetButtonDown("X_RightButton");
+        }
     }
 
     //D-pad
@@ -58,10 +71,20 @@ public static class InputManager
 
     //Triggers and bumpers
     public static bool X_LeftBump() {
-        return Input.GetButtonDown("X_LeftBump");
+        if (ToggleHold) {
+            return Input.GetButton("X_LeftBump");
+        }
+        else {
+            return Input.GetButtonDown("X_LeftBump");
+        }
     }
     public static bool X_RightBump() {
-        return Input.GetButtonDown("X_RightBump");
+        if (ToggleHold) {
+            return Input.GetButton("X_RightBump");
+        }
+        else {
+            return Input.GetButtonDown("X_RightBump");
+        }
     }
     public static float X_LeftTrigger() {
         float X = 0;
@@ -76,22 +99,52 @@ public static class InputManager
 
     //Buttons
     public static bool X_AButton() {
-        return Input.GetButtonDown("X_AButton");
+        if (ToggleHold) {
+            return Input.GetButton("X_AButton");
+        }
+        else {
+            return Input.GetButtonDown("X_AButton");
+        }
     }
     public static bool X_BButton() {
-        return Input.GetButtonDown("X_BButton");
+        if (ToggleHold) {
+            return Input.GetButton("X_BButton");
+        }
+        else {
+            return Input.GetButtonDown("X_BButton");
+        }
     }
     public static bool X_XButton() {
-        return Input.GetButtonDown("X_XButton");
+        if (ToggleHold) {
+            return Input.GetButton("X_XButton");
+        }
+        else {
+            return Input.GetButtonDown("X_XButton");
+        }
     }
     public static bool X_YButton() {
-        return Input.GetButtonDown("X_YButton");
+        if (ToggleHold) {
+            return Input.GetButton("X_YButton");
+        }
+        else {
+            return Input.GetButtonDown("X_YButton");
+        }
     }
     public static bool X_BackButton() {
-        return Input.GetButtonDown("X_BackButton");
+        if (ToggleHoldExtras) {
+            return Input.GetButton("X_StartButton");
+        }
+        else {
+            return Input.GetButtonDown("X_StartButton");
+        }
     }
     public static bool X_StartButton() {
-        return Input.GetButtonDown("X_StartButton");
+        if (ToggleHoldExtras) {
+            return Input.GetButton("X_StartButton");
+        }
+        else {
+            return Input.GetButtonDown("X_StartButton");
+        }
     }
     #endregion
 
@@ -120,10 +173,20 @@ public static class InputManager
 
     //Joystick Buttons
     public static bool PS_LeftButton() {
-        return Input.GetButtonDown("PS_LeftButton");
+        if (ToggleHold) {
+            return Input.GetButton("PS_LeftButton");
+        }
+        else {
+            return Input.GetButtonDown("PS_LeftButton");
+        }
     }
     public static bool PS_RightButton() {
-        return Input.GetButtonDown("PS_RightButton");
+        if (ToggleHold) {
+            return Input.GetButton("PS_RightButton");
+        }
+        else {
+            return Input.GetButtonDown("PS_RightButton");
+        }
     }
 
     //D-pad
@@ -140,10 +203,20 @@ public static class InputManager
 
     //Triggers and bumpers
     public static bool PS_LeftBump() {
-        return Input.GetButtonDown("PS_LeftBump");
+        if (ToggleHold) {
+            return Input.GetButton("PS_LeftBump");
+        } 
+        else {
+            return Input.GetButtonDown("PS_LeftBump");
+        }
     }
     public static bool PS_RightBump() {
-        return Input.GetButtonDown("PS_RightBump");
+        if (ToggleHold) {
+            return Input.GetButton("PS_RightBump");
+        }
+        else {
+            return Input.GetButtonDown("PS_RightBump");
+        }
     }
     public static float PS_LeftTrigger() {
         float X = 0;
@@ -158,25 +231,60 @@ public static class InputManager
 
     //Buttons
     public static bool PS_SButton() {
-        return Input.GetButtonDown("PS_SButton");
+        if (ToggleHold) {
+            return Input.GetButton("PS_SButton");
+        }
+        else {
+            return Input.GetButtonDown("PS_SButton");
+        }
     }
     public static bool PS_XButton() {
-        return Input.GetButtonDown("PS_XButton");
+        if (ToggleHold) {
+            return Input.GetButton("PS_XButton");
+        }
+        else {
+            return Input.GetButtonDown("PS_XButton");
+        }
     }
     public static bool PS_OButton() {
-        return Input.GetButtonDown("PS_OButton");
+        if (ToggleHold) {
+            return Input.GetButton("PS_OButton");
+        }
+        else {
+            return Input.GetButtonDown("PS_OButton");
+        }
     }
     public static bool PS_TButton() {
-        return Input.GetButtonDown("PS_TButton");
+        if (ToggleHold) {
+            return Input.GetButton("PS_TButton");
+        }
+        else {
+            return Input.GetButtonDown("PS_TButton");
+        }
     }
     public static bool PS_ShareButton() {
-        return Input.GetButtonDown("PS_ShareButton");
+        if (ToggleHoldExtras) {
+            return Input.GetButton("PS_ShareButton");
+        }
+        else {
+            return Input.GetButtonDown("PS_ShareButton");
+        }
     }
     public static bool PS_OptionsButton() {
-        return Input.GetButtonDown("PS_OptionsButton");
+        if (ToggleHoldExtras) {
+            return Input.GetButton("PS_OptionsButton");
+        }
+        else {
+            return Input.GetButtonDown("PS_OptionsButton");
+        }
     }
     public static bool PS_HomeButton() {
-        return Input.GetButtonDown("PS_HomeButton");
+        if (ToggleHoldExtras) {
+            return Input.GetButton("PS_HomeButton");
+        }
+        else {
+            return Input.GetButtonDown("PS_HomeButton");
+        }
     }
     #endregion
 }
