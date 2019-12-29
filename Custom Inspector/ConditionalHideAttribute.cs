e6bd6f395b58be5cc231dad18ba0d94ab8d93207
Null Cookie Games <31669166+PeterTranz@users.noreply.github.com>
2019-12-29 11:14:32 -0500

    Conditional Enum Hide

--- Custom Inspector/ConditionalHideAttribute.cs
+++ Custom Inspector/ConditionalHideAttribute.cs
@@ -20,3 +20,28 @@ public class ConditionalHideAttribute : PropertyAttribute {
         this.HideInInspector = hideInInspector;
     }
 }
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property |
+    AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
+public class ConditionalEnumHideAttribute : PropertyAttribute {
+    //The name of the bool field that will be in control
+    public string ConditionalSourceField = "";
+
+    public int EnumValue1 = 0;
+    public int EnumValue2 = 0;
+
+    public bool HideInInspector = false;
+    public bool Inverse = false;
+
+    public ConditionalEnumHideAttribute(string conditionalSourceField, int enumValue1) {
+        this.ConditionalSourceField = conditionalSourceField;
+        this.EnumValue1 = enumValue1;
+        this.EnumValue2 = enumValue1;
+    }
+
+    public ConditionalEnumHideAttribute(string conditionalSourceField, int enumValue1, int enumValue2) {
+        this.ConditionalSourceField = conditionalSourceField;
+        this.EnumValue1 = enumValue1;
+        this.EnumValue2 = enumValue2;
+    }
+}
