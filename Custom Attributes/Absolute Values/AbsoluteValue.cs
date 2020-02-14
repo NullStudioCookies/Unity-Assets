64565ad2735a61c351f1d30151b2bbbdd65d2557
Null Cookie Games <31669166+PeterTranz@users.noreply.github.com>
2020-02-14 11:09:28 -0500

    Added absolute value attribute

---
+++ Custom Attributes/Absolute Values/AbsoluteValue.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is left empty, it is only used for the attribute name.
+/// </summary>
+public class AbsoluteValue : PropertyAttribute {
+
+}
---
+++ Custom Attributes/Absolute Values/AbsoluteValuePropertyDrawer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// The logic of the attribute "AbsoluteValue" is simple.
+/// it just checks if its a integer, float or somthing else.
+/// It will simply make any negative number into positive using
+/// Mathf.abs.
+/// </summary>
+[CustomPropertyDrawer(typeof(AbsoluteValue))]
+public class AbsoluteValuePropertyDrawer : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        if (property.propertyType == SerializedPropertyType.Integer) {
+            property.intValue = Mathf.Abs(EditorGUI.IntField(position, label, property.intValue));
+        }
+        else if (property.propertyType == SerializedPropertyType.Float) {
+            property.floatValue = Mathf.Abs(EditorGUI.FloatField(position, label, property.floatValue));
+        }
+        else {
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+}
