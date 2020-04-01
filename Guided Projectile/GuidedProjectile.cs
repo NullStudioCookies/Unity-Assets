ce81db62f568ccb01c5f553db6b7556a3cc585ff
Null Cookie Games <31669166+PeterTranz@users.noreply.github.com>
2020-03-31 23:13:48 -0400

    GuidedProjectile is more dynamic

--- Guided Projectile/GuidedProjectile.cs
+++ Guided Projectile/GuidedProjectile.cs
@@ -3,22 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 enum GuidanceSystem { Controlled, Homing}
+enum ForwardAxis { X_Axis, Y_Axis, Z_Axis}
 [RequireComponent(typeof(Rigidbody))]
 public class GuidedProjectile : MonoBehaviour {
     [SerializeField] GuidanceSystem GuidanceMethod;
+    [Tooltip("This is used to set which way the projectile is facing.")][SerializeField] ForwardAxis ForwardDirection;
     [SerializeField][AbsoluteValue()] float TurnRate = 0;
+
     [SerializeField] bool SetInitalVelocity = false;
     [ConditionalHide("SetInitalVelocity", true)][SerializeField] float ProjectileVelocity = 0;
 
-    [Header("Control guided variables")][Space(5)]
-    [ConditionalEnumHide("GuidanceMethod", 0)][SerializeField] string XInputAxis;
-    [ConditionalEnumHide("GuidanceMethod", 0)][SerializeField] string YInputAxis;
-    [ConditionalEnumHide("GuidanceMethod", 0)][SerializeField] float InputSensitivity = 0;
-    [ConditionalEnumHide("GuidanceMethod", 0)][SerializeField] float InputSmoothing = 0;
+    [Header("Control Guided Variables")][Space(5)]
+    [ConditionalEnumHide("GuidanceMethod", (int)GuidanceSystem.Controlled)][SerializeField] string XInputAxis = null;
+    [ConditionalEnumHide("GuidanceMethod", (int)GuidanceSystem.Controlled)][SerializeField] string YInputAxis = null;
+    [ConditionalEnumHide("GuidanceMethod", (int)GuidanceSystem.Controlled)][SerializeField] float InputSensitivity = 0;
+    [ConditionalEnumHide("GuidanceMethod", (int)GuidanceSystem.Controlled)][SerializeField] float InputSmoothing = 0;
     Vector3 SmoothVector, TargetDirection;
 
     Rigidbody ProjectileBody;
-    GameObject Target;
+    public GameObject Target;
+
+    [ExecuteInEditMode]
+    void OnValidate() {
+        if (ProjectileVelocity < 0) {
+            ProjectileVelocity = 0;
+        }
+        if (InputSensitivity < 0) {
+            InputSensitivity = 0;
+        }
+        if (InputSmoothing < 0) {
+            InputSmoothing = 0;
+        }
+    }
 
     // Start is called before the first frame update
     void Start() {
@@ -27,11 +43,10 @@ public class GuidedProjectile : MonoBehaviour {
             ProjectileVelocity = ProjectileBody.velocity.magnitude;
         }
         else {
-            ProjectileBody.velocity = ProjectileVelocity * Vector3.forward;
+            ApplyNewDirection(Quaternion.identity);
         }
     }
 
-    // Fixed Update is called during the physics calculations
     void FixedUpdate() {
         switch (GuidanceMethod) {
             case GuidanceSystem.Controlled:
@@ -44,7 +59,6 @@ public class GuidedProjectile : MonoBehaviour {
     }
 
     void ControlProjectile() {
-        // Creating a direction based on the input
         var NewDirection = new Vector2(-Input.GetAxis(YInputAxis), Input.GetAxis(XInputAxis));
 
         NewDirection = Vector2.Scale(NewDirection, new Vector2(InputSensitivity * InputSmoothing, InputSensitivity * InputSmoothing));
@@ -52,19 +66,31 @@ public class GuidedProjectile : MonoBehaviour {
         SmoothVector.y = Mathf.Lerp(SmoothVector.y, NewDirection.y, 1f / InputSmoothing);
         TargetDirection += SmoothVector;
 
-        // Rotate projectil towards the new direction and appling force
-        ProjectileBody.MoveRotation(Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(TargetDirection), TurnRate));
-        ProjectileBody.velocity = transform.forward * ProjectileVelocity;
+        ApplyNewDirection(Quaternion.Euler(TargetDirection));
     }
 
     void HomingTarget() {
-        // Rotate projectile towards the target and while applying force
-        Quaternion TargetRotation = Quaternion.LookRotation(Target.transform.position - this.transform.position);
-        ProjectileBody.MoveRotation(Quaternion.RotateTowards(transform.rotation, TargetRotation, TurnRate));
-        ProjectileBody.velocity = transform.forward * ProjectileVelocity;
+        Quaternion TargetDirection = Quaternion.LookRotation(Target.transform.position - this.transform.position);
+
+        ApplyNewDirection(TargetDirection);
     }
 
-    // Logic for setting a target for homing guidence system
+    void ApplyNewDirection(Quaternion TargetRotation) {
+        ProjectileBody.MoveRotation(Quaternion.RotateTowards(transform.rotation, TargetRotation, TurnRate));
+
+        switch (ForwardDirection) {
+            case ForwardAxis.X_Axis:
+                ProjectileBody.velocity = transform.right * ProjectileVelocity;
+                break;
+            case ForwardAxis.Y_Axis:
+                ProjectileBody.velocity = transform.up * ProjectileVelocity;
+                break;
+            case ForwardAxis.Z_Axis:
+                ProjectileBody.velocity = transform.forward * ProjectileVelocity;
+                break;
+        }
+    }
+
     public void SetTarget(GameObject TargetObject = null) {
         Target = TargetObject;
     }
