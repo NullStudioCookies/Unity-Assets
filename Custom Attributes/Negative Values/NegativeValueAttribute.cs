4f8b382f7ebd55d65212f918277e0f8748339c74
Null Cookie Games <31669166+PeterTranz@users.noreply.github.com>
2020-05-11 09:19:08 -0400

    Negative Values attribute
    allows any given value with this tag to be negative only.

---
+++ Custom Attributes/Negative Values/NegativeValueAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is left empty, it is only used for the attribute name.
+/// </summary>
+
+public class NegativeValueAttribute : PropertyAttribute {
+
+}
---
+++ Custom Attributes/Negative Values/NegativeValuePropertyDrawer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// The logic of the attribute "AbsoluteValue" is simple.
+/// it just checks if its a integer, float or somthing else.
+/// It will simply make any positive number and multiplying
+/// it with a -1.
+/// </summary>
+
+
+[CustomPropertyDrawer(typeof(NegativeValueAttribute))]
+public class NegativeValuePropertyDrawer : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        if (property.propertyType == SerializedPropertyType.Integer) {
+            if (property.intValue > 0) {
+                property.intValue *= -1;
+            }
+
+            property.intValue = EditorGUI.IntField(position, label, property.intValue);
+        }
+        else if (property.propertyType == SerializedPropertyType.Float) {
+            if (property.floatValue > 0) {
+                property.floatValue *= -1;
+            }
+
+            property.floatValue = EditorGUI.FloatField(position, label, property.floatValue);
+        }
+        else {
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+}
