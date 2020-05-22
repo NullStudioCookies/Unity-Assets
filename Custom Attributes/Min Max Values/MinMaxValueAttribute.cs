53085b68d0f6a6b131af27f2a5d6bd6ebd249791
Null Studio Cookies <31669166+PeterTranz@users.noreply.github.com>
2020-05-21 23:38:01 -0400

    Min Max Value Attribute

---
+++ Custom Attributes/Min Max Values/MinMaxValueAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script contains the attribute tags for the min and max value
+/// validators. Each on takes 1 argument to set the limit for the 
+/// variable.
+/// </summary>
+
+// This class does nothing
+class MinMaxValueAttribute {
+    
+}
+
+public class MinValueAttribute : PropertyAttribute {
+    public float minValue { get; private set; }
+
+    public MinValueAttribute(float MainVlaue) {
+        minValue = MainVlaue;
+    }
+
+    public MinValueAttribute(int MinVlaue) {
+        minValue = MinVlaue;
+    }
+}
+
+public class MaxValueAttribute : PropertyAttribute {
+    public float maxValue { get; private set; }
+
+    public MaxValueAttribute(float MaxVlaue) {
+        maxValue = MaxVlaue;
+    }
+
+    public MaxValueAttribute(int MaxVlaue) {
+        maxValue = MaxVlaue;
+    }
+}
---
+++ Custom Attributes/Min Max Values/MinMaxValuePropertyDrawer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// This script limits the value for the variables based on the 
+/// attribute that was used.
+/// </summary>
+
+// This class does nothing
+class MinMaxValuePropertyDrawer {
+    
+}
+
+[CustomPropertyDrawer(typeof(MinValueAttribute))]
+public class MinValueAttributePropertyDrawer : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        MinValueAttribute Min = (MinValueAttribute)attribute;
+
+        if (property.propertyType == SerializedPropertyType.Integer) {
+            if (property.intValue < Min.minValue) {
+                property.intValue = (int)Min.minValue;
+            }
+
+            property.intValue = EditorGUI.IntField(position, label, property.intValue);
+        }
+        else if (property.propertyType == SerializedPropertyType.Float) {
+            if (property.floatValue < Min.minValue) {
+                property.floatValue = (float)Min.minValue;
+            }
+
+            property.floatValue = EditorGUI.FloatField(position, label, property.floatValue);
+        }
+        else {
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+}
+
+[CustomPropertyDrawer(typeof(MaxValueAttribute))]
+public class MaxValueAttributePropertyDrawer : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        MaxValueAttribute Max = (MaxValueAttribute)attribute;
+
+        if (property.propertyType == SerializedPropertyType.Integer) {
+            if (property.intValue > Max.maxValue) {
+                property.intValue = (int)Max.maxValue;
+            }
+
+            property.intValue = EditorGUI.IntField(position, label, property.intValue);
+        }
+        else if (property.propertyType == SerializedPropertyType.Float) {
+            if (property.floatValue > Max.maxValue) {
+                property.floatValue = (float)Max.maxValue;
+            }
+
+            property.floatValue = EditorGUI.FloatField(position, label, property.floatValue);
+        }
+        else {
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+}
