bf15e1cbee3e59f63df59d64b139e74e92f545b3
Null Studio Cookies <31669166+PeterTranz@users.noreply.github.com>
2020-08-07 13:17:14 -0400

    Enum Flags Attribute
    New custom attributes

---
+++ Custom Attributes/Enum Flags/EnumFlagsAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// This script is left empty, it is only used for the attribute name.
+/// </summary>
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property |
+    AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
+public class EnumFlagsAttribute : PropertyAttribute {
+    public EnumFlagsAttribute() { }
+}
---
+++ Custom Attributes/Enum Flags/EnumFlagsPropertyDrawer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// This script is the logic for creating a drop down field to
+/// select multiple enum values.
+/// </summary>
+
+[CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
+public class EnumFlagsPropertyDrawer : PropertyDrawer {
+    public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label) {
+        _property.intValue = EditorGUI.MaskField(_position, _label, _property.intValue, _property.enumNames);
+    }
+}
