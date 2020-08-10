d9441d43917dffeced9dda4c0be052c6c0cf34ca
Null Studio Cookies <31669166+PeterTranz@users.noreply.github.com>
2020-08-10 14:19:23 -0400

    Read Only Attribute

---
+++ Custom Attributes/Read Only/ReadOnlyAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script does nothing it is only used for the attribute tag.
+/// </summary>
+public class ReadOnlyAttribute : PropertyAttribute {
+
+}
---
+++ Custom Attributes/Read Only/ReadOnlyPropertyDrawer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// This script makes it so that variables with this tag can be seen
+/// but not changed.
+/// </summary>
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyPropertyDrawer : PropertyDrawer {
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
